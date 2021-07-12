66414a690e482117dd0a6fd538a2bc70d046bff1
s4tenko <s4tenko@gmail.com>
2021-07-12 09:56:27 +0300

    Query changes

--- goods-supply.session.sql
+++ goods-supply.session.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS "goods" CASCADE;
 CREATE TABLE "goods" (
   "id" SERIAL PRIMARY KEY,
   "item_name" VARCHAR(256) NOT NULL CHECK("item_name" != '') UNIQUE,
-  "price" INTEGER NOT NULL CHECK("price" > 0)
+  "item_price" INTEGER NOT NULL CHECK("price" > 0)
 );
 /* Creating ORDERS table */
 DROP TABLE IF EXISTS "orders" CASCADE;
@@ -14,7 +14,7 @@ CREATE TABLE "orders" (
   "customer_phone" VARCHAR(16) NOT NULL CHECK("customer_phone" != ''),
   "contract_number" VARCHAR(512) NOT NULL CHECK("contract_number" != ''),
   "contract_date" DATE NOT NULL DEFAULT CURRENT_DATE,
-  "product_name" TEXT REFERENCES "goods" ("item_name"),
+  "product_name" TEXT NOT NULL REFERENCES "goods" ("item_name"),
   "product_quantity" INTEGER NOT NULL CHECK("product_quantity" > 0),
   UNIQUE ("product_name", "contract_number")
 );
@@ -22,8 +22,7 @@ CREATE TABLE "orders" (
 DROP TABLE IF EXISTS "shipment" CASCADE;
 CREATE TABLE "shipment" (
   "id" SERIAL PRIMARY KEY,
-  "order_id" INTEGER REFERENCES "orders" ("id"),
-  "date" DATE NOT NULL DEFAULT CURRENT_DATE,
-  "goods_quantity" INTEGER NOT NULL CHECK("goods_quantity" > 0),
-  UNIQUE("order_id", "date")
+  "order_id" INTEGER NOT NULL REFERENCES "orders" ("id"),
+  "shipment_date" DATE NOT NULL DEFAULT CURRENT_DATE,
+  "goods_quantity" INTEGER NOT NULL CHECK("goods_quantity" > 0)
 );
